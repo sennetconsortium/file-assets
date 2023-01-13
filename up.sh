4f98324b0d908040bdb41bce571af16356ab1a3e
SamSedivy <sls325@pitt.edu>
2023-01-13 16:03:51 -0500

    Separate local and dev docker-compose

--- up.sh
+++ up.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-docker-compose up -d
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied. Please supply the environment (local or dev)"
+    exit 1
+fi
+environment=$1
+if [ "$environment" == "local" ]; then
+  docker-compose -f docker-compose.yml -f docker-compose.local.yml up -d
+elif [ "$environment" == "dev" ]; then
+  docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+else
+  echo "Invalid environment specified ($1)"
+fi
